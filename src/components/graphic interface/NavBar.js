d2a900c508d4b92df1366608318d02a272b1c512
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-03-12 01:34:27 +0100

    Changed minor details in NavBar

--- src/components/graphic interface/NavBar.js
+++ src/components/graphic interface/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Login from "../services/login/Login.js";
 import { LoggedOut } from '@solid/react';
 import Dashboard from '../services/dashboard/Dashboard.js';
+import { Link } from '@material-ui/core';
+import "typeface-roboto";
 
 const NavBar = () => {
     const classes = useStyles();
@@ -15,9 +16,9 @@ const NavBar = () => {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar >
-                    <Typography variant="h6" className={classes.title}>viade_en3b</Typography>
-                    <Dashboard/>
-                    <LoggedOut><Button href={"#/register"} color="inherit">Sign up</Button></LoggedOut>
+                    <Link underline='none' color='inherit' href={'#/welcome'} className={classes.title}>Viade</Link>
+                    <Dashboard className={classes.menuButton}/>
+                    <LoggedOut><Button className={classes.menuButton} href={"#/register"} color="inherit">Sign up</Button></LoggedOut>
                     <Login/>
                 </Toolbar>
             </AppBar>
@@ -34,6 +35,8 @@ const useStyles = makeStyles(theme => ({
     },
     title: {
       flexGrow: 1,
+      fontSize: 20,
+      letterSpacing: 1,
     },
 }));
 
