7f18de9b213a6fb1087171a2983b97ebc2a284ed
Elena <UO264329@uniovi.es>
2020-03-24 20:49:01 +0100

    MapSnapshot component created
    Based on demoMap

---
+++ src/components/map/MapSnapshot.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup,Polyline } from 'react-leaflet';
+import * as data from "../../demoData/demoRoute.json";
+
+export default class MapSnapshot extends Component {
+  points = data.features.map(point => (
+    [point.geometry.coordinates[0],point.geometry.coordinates[1]]
+    ));
+  
+  setPopup = () =>{}
+
+  render() {
+    return (
+      <Map bounds={this.points} doubleClickZoom={false} dragging={false} keyboard={false}
+       scrollWheelZoom={false} tap={false} touchZoom={false} zoomControl={false} boxZoom={false}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Polyline   positions={data.features.map(point => (
+          [point.geometry.coordinates[0],point.geometry.coordinates[1]]
+          ))} color='blue'/>
+        ))}
+      </Map>
+    )
+  }
+}
