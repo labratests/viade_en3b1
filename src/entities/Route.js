e5d5f1f49ddcb5c5feb2e55ebd1fe0e23895d6bf
Carmen <uo264045@uniovi.es>
2020-03-26 00:40:21 +0100

    total distance created

--- src/entities/Route.js
+++ src/entities/Route.js
@@ -24,6 +24,30 @@ class Route{
         return this.routeElements;
     }
 
+    getTotalDistance(){
+        var total = 0;
+        var aux = 0;
+        
+        for (let i = 1; i < this.routeElements.length; i++) {
+            aux = this.routeElements[i].getLatitude() - this.routeElements[i-1].getLatitude();
+            total += this.addDistance(aux);
+            aux = this.routeElements[i].getLongitude() - this.routeElements[i-1].getLongitude();
+            total += this.addDistance(aux);
+            aux = this.routeElements[i].getElevation() - this.routeElements[i-1].getElevation();
+            total += this.addDistance(aux);
+        }
+
+        return total;
+    }
+
+    addDistance(aux){
+        if(aux < 0){
+            return -aux;
+        } else {
+            return aux;
+        }
+    }
+
     getComments(){
         return this.comments;
     }
