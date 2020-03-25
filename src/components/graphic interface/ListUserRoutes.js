40c611bd126fecde9c50709a43ae6e5fd513e665
Elena <UO264329@uniovi.es>
2020-03-26 00:56:26 +0100

    designg of card rearranged, dinamic number of cards

--- src/components/graphic interface/ListUserRoutes.js
+++ src/components/graphic interface/ListUserRoutes.js
@@ -14,25 +14,25 @@ export default function ListUserRoutes() {
   const [spacing] = React.useState(2);
   const classes = useStyles();
 
+
+
+  let size=[];
+  for (let index = 0; index < 5; index++) {
+     size.push(1); 
+  }
+
+  
   return (
     <Grid container className={classes.root} spacing={2}>
       <Grid item xs={12}>
         <Grid container justify="center" spacing={spacing}>
+        {size.map(card => (
           <Grid item>
-            <LoggedIn>
-              <MyRouteCard />
-            </LoggedIn>
-          </Grid>
-          <Grid item>
-            <LoggedIn>
-              <MyRouteCard />
-            </LoggedIn>
-          </Grid>
-          <Grid item>
-            <LoggedIn>
-              <MyRouteCard />
-            </LoggedIn>
-          </Grid>
+          <LoggedIn>
+            <MyRouteCard />
+          </LoggedIn>
+        </Grid>
+        ))}
         </Grid>
       </Grid>
     </Grid>
