b06b5c2a730e91c021d0b5d209a7ab0b4bcb5b7d
Elena <UO264329@uniovi.es>
2020-03-31 00:31:01 +0200

    error patched but not solved

--- src/parser/LoadFromPod.js
+++ src/parser/LoadFromPod.js
@@ -12,13 +12,23 @@ class LoadFromPod{
         var viadeRoutes = userWebId.slice(0, userWebId.length - 16) + "/public/viade/routes";
         var files = [];
 
-        if(await fc.itemExists(viadeRoutes)){
-            files = await fc.readFolder(viadeRoutes).files;
+        try {
+            if(await fc.itemExists(viadeRoutes)){
+                files = await fc.readFolder(viadeRoutes).files;
+            }
+            else{   
+                files.forEach(file => {
+                    user.addRoute(parser.parse(file));
+                });
+            } 
+        }
+        catch(error) {
+            console.log( error )         // A full error response 
+            console.log( error.status )  // Just the status code of the error
+            console.log( error.message ) // Just the status code and statusText
         }
 
-        files.forEach(file => {
-            user.addRoute(parser.parse(file));
-        });
+        
     }
 
     async loadFile(url){
