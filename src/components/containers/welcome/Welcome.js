ff1080fbc5ea59ca78857a75a9985d4d9d2636f6
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-04-02 02:16:26 +0200

    Added logo

--- src/components/containers/welcome/Welcome.js
+++ src/components/containers/welcome/Welcome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from '../../graphic interface/NavBar.js';
 import { Typography, Grid, Divider } from '@material-ui/core';
 import 'typeface-roboto';
+import logo1 from '../../../assets/img/logo/logo1.svg';
 
 const Welcome = () => {
 
@@ -14,13 +15,16 @@ const Welcome = () => {
                 direction="column"
                 alignItems="center"
                 justify="center"
-                style={{ marginTop: '40vh' }}
+                style={{ marginTop: '30vh' }}
             >
+                <Grid item xs={3} style={{marginBottom: '2vh'}}>
+                    <img src={logo1} alt="Viade Logo" style={{ height: '10vh', width: '10vh' }} />
+                </Grid>
 
                 <Grid item xs={3}>
-                    <Typography style={{marginBottom: '10px'}} variant="h4" color="inherit" align="center">Welcome to Viade.</Typography>
+                    <Typography style={{ marginBottom: '10px' }} variant="h4" color="inherit" align="center">Welcome to Viade.</Typography>
                     <Divider />
-                    <Typography style={{marginTop: '10px'}} variant="body1" color="inherit" align="center">The first routes management system based on SOLID technology.</Typography>
+                    <Typography style={{ marginTop: '10px' }} variant="body1" color="inherit" align="center">The first routes management system based on SOLID technology.</Typography>
                 </Grid>
 
             </Grid>
