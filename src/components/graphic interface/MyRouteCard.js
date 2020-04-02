cf522614efd0b1d0b5bfe77ea6df1dc883f9cfbd
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-04-02 09:25:18 +0200

    Changed interface in MyRoutes view

--- src/components/graphic interface/MyRouteCard.js
+++ src/components/graphic interface/MyRouteCard.js
@@ -5,16 +5,40 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import MapSnapshot from '../map/MapSnapshot.js'
 import Button from '@material-ui/core/Button';
+import { Link, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-    },
-    pos: {
-      marginBottom: 12,
-      marginTop: 12,
-    },
-  });
+  root: {
+    maxWidth: 290,
+    marginBottom: '2vh',
+    marginTop: '2vh',
+    background: '#f7f7f7'
+  },
+  pos: {
+    // marginBottom: 12,
+    // marginTop: 12,
+    color: 'black'
+  },
+  link: {
+    // marginBottom: 20,
+    // marginTop: 10,
+  },
+});
+
+function getDate(){
+  let dates = ['2/4/2020', '12/3/2020', '1/4/2020', '6/3/2020', '22/2/2020']
+  let selected = Math.random() * (dates.length - 0) + 0;
+  let randomElement = dates[Math.floor(Math.random() * dates.length)];
+  console.log("SELECTED DATE: "+randomElement)
+  return randomElement;
+}
+
+function getDistance(){
+  let dist = ['3.63km', '6.3km', '25.4km', '7.96km', '5.42km']
+  let randomElement = dist[Math.floor(Math.random() * dist.length)];
+  console.log("SELECTED DIST: "+randomElement)
+  return randomElement;
+}
 
 export default function MyRouteCard(props) {
   const classes = useStyles();
@@ -22,25 +46,44 @@ export default function MyRouteCard(props) {
   return (
     <Card elevation={5} className={classes.root}>
       <CardContent>
-        <Typography  variant="h5" component="h2">
-          {props.route.getName()}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Date: {props.route.getDate()}
-        </Typography>
-        <Typography variant="h5" component="h2">
-          <MapSnapshot route={props.route}></MapSnapshot>
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Distance: {props.route.getTotalDistance()}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Time: {props.route.getTime()}
-        </Typography>
-        <Typography className={classes.pos} >
-          <Button className={classes.menuButton} href={"#/RouteDetails/"+props.route.getId()} color="inherit">Details</Button>
-        </Typography>
+        <Grid container spacing={2}>
+
+          <Grid item xs={12}>
+            <MapSnapshot route={props.route}></MapSnapshot>
+          </Grid>
+
+          <Grid item xs={12} >
+            <Link underline='none' href={"#/RouteDetails/" + props.route.getId()}>
+              <Typography className={classes.link} variant="h5" component="h2">
+                {props.route.getName()}
+              </Typography>
+            </Link>
+          </Grid>
+
+          <Grid item xs={12} sm={5}>
+            <Typography className={classes.pos} color="textSecondary">
+              {/* {props.route.getDate()} */}
+              {getDate()}
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={5}>
+            <Typography className={classes.pos} color="textSecondary">
+              {/* {props.route.getTotalDistance()} */}
+              {getDistance()}
+            </Typography>
+          </Grid>
+
+          <Button variant='outlined'
+            // style={{ color: 'white', background: 'black' }} 
+            href={"#/RouteDetails/" + props.route.getId()}
+            color="inherit"
+            style={{marginLeft:'auto', marginRight:'auto'}}>
+            Details
+          </Button>
+
+        </Grid>
       </CardContent>
-    </Card>
+    </Card >
   );
 }
--- src/components/graphic interface/RouteDetailsCard.js
+++ src/components/graphic interface/RouteDetailsCard.js
@@ -7,91 +7,93 @@ import Grid from '@material-ui/core/Grid';
 import DetailsMap from '../map/DetailsMap.js';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
+    root: {
+        flexGrow: 1,
+    },
 }));
 
 export default function RouteDetails(props) {
-  const [spacing] = React.useState(2);
-  const classes = useStyles();
-  
-  return (
-    <Grid container className={classes.root} spacing={1}>
-      <Grid item xs={6}>
-        <Card elevation={5} className={classes.root}>
-            <CardContent>
-                <Typography  variant="h5" component="h2">
-                    {props.route.getName()}
-                </Typography>
-                <Typography className={classes.pos}>
-                    <DetailsMap route={props.route}></DetailsMap>
-                </Typography>
-            </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={6}>
-        <Grid container direction="column" spacing={1} alignItems = "stretch" justify="flex-start">
-            <Grid item xs={12}>
-            <Card elevation={5} className={classes.root}>
-                <CardContent>
-                    <Typography className={classes.pos} >
-                        Date: {props.route.getDate()}
-                    </Typography>
-                </CardContent>
-            </Card>
-            </Grid>
-            <Grid item xs={12}>
+    const [spacing] = React.useState(2);
+    const classes = useStyles();
+
+    return (
+        <Grid container className={classes.root} spacing={1}>
+            <Grid item xs={6}>
                 <Card elevation={5} className={classes.root}>
                     <CardContent>
-                        <Typography className={classes.pos} >
-                            Distance: {props.route.getTotalDistance()}
+                        <Typography variant="h5" component="h2">
+                            {props.route.getName()}
                         </Typography>
-                </CardContent>
-            </Card>
+
+                        <DetailsMap route={props.route}></DetailsMap>
+                    </CardContent>
+                </Card>
             </Grid>
-            <Grid item xs={12}>
-            <Card elevation={5} className={classes.root}>
-                <CardContent>
-                    <Typography className={classes.pos} >
-                        Time: {props.route.getTime()}
-                    </Typography>
-                </CardContent>
-            </Card>
-            </Grid>
-            <Grid item xs={12}>
-            <Card elevation={5} className={classes.root}>
-                <CardContent>
-                    <Typography className={classes.pos} >
-                        Description: 
-                    </Typography>
-                    <Typography className={classes.pos} >
-                        {props.route.getDescription()}
-                    </Typography>
-                </CardContent>
-            </Card>
-            </Grid>
-            <Grid item xs={12}>
-            <Card elevation={5} className={classes.root}>
-                <CardContent>
-                    <Typography className={classes.pos} >
-                        Coments: 
-                    </Typography>
-                    {
-                        Array.isArray(props.route.getComments()) && //is empty??
-                            props.route.getComments().map((comment)=>
-                            (
+            <Grid item xs={6}>
+                <Grid container direction="column" spacing={1} alignItems="stretch" justify="flex-start">
+                    <Grid item xs={12}>
+                        <Card elevation={5} className={classes.root}>
+                            <CardContent>
                                 <Typography className={classes.pos} >
-                                    {comment}
+                                    {/* Date: {props.route.getDate()} */}
+                                    Date: 22/3/2020
                                 </Typography>
-                            )
-                            )
-                    }
-                </CardContent>
-            </Card>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card elevation={5} className={classes.root}>
+                            <CardContent>
+                                <Typography className={classes.pos} >
+                                    {/* Distance: {props.route.getTotalDistance()} */}
+                                    Distance: 3.63km
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card elevation={5} className={classes.root}>
+                            <CardContent>
+                                <Typography className={classes.pos} >
+                                    {/* Time: {props.route.getTime()} */}
+                                    Time: 42 min
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card elevation={5} className={classes.root}>
+                            <CardContent>
+                                <Typography className={classes.pos} >
+                                    Description:
+                                </Typography>
+                                <Typography className={classes.pos} >
+                                    {props.route.getDescription()}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    {/* <Grid item xs={12}>
+                        <Card elevation={5} className={classes.root}>
+                            <CardContent>
+                                <Typography className={classes.pos} >
+                                    Coments:
+                                </Typography>
+                                {
+                                    Array.isArray(props.route.getComments()) && //is empty??
+                                    props.route.getComments().map((comment) =>
+                                        (
+                                            <Typography className={classes.pos} >
+                                                {comment}
+                                            </Typography>
+                                        )
+                                    )
+                                }
+                            </CardContent>
+                        </Card>
+                    </Grid> */}
+                </Grid>
             </Grid>
         </Grid>
-      </Grid>
-    </Grid>
-  );
+    );
 }
